edbda1c3634584bb02818dec7aad25ba158032b0
iamdudumon <kksy4511@naver.com>
2024-06-18 02:27:58 +0900

    feat: users crud 추가

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  private users: Array<User> = [];
+  private id = 0;
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    this.users.push({
+      id: ++this.id,
+      ...createUserDto,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.users;
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    const user: User = this.users.find((user) => user.id == id);
+
+    if (!user) throw new NotFoundException();
+    return user;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    const user: User = this.findOne(id);
+
+    this.remove(id);
+    this.users.push({
+      ...user,
+      ...updateUserDto,
+      updatedAt: new Date(),
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    const idx: number = this.users.findIndex((user) => user.id == id);
+
+    this.users.splice(idx, 1);
   }
 }
