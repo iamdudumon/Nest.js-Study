e53479a254a28e7b14f3b6ec1e781eaa6efaa16f
iamdudumon <kksy4511@naver.com>
2025-05-04 23:59:50 +0900

    feat: TypeORM을 활용한 PostgreSQL 연동 설정 추가
    - TypeOrmModule.forRoot를 통해 DB 연결 설정
    - DB 접속 정보는 .env 파일로 분리하고 @nestjs/config 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
@@ -6,7 +9,24 @@ import { BoardsModule } from './boards/boards.module';
 import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [UsersModule, BoardsModule, CommentsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity.ts'],
+      synchronize: true,
+    }),
+    UsersModule,
+    BoardsModule,
+    CommentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
