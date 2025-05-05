60af6e48ed5b0205c0a582b98591dd3d60441985
iamdudumon <kksy4511@naver.com>
2025-05-05 23:27:00 +0900

    feat: 요청 URL 및 처리시간을 기록하는 로깅 인터셉터 추가
    - "[Request] {method} {url} [{statusCode}] - {duration}ms" 포맷으로 logging
    - LoggingInterceptor에서 TestLogger 사용하도록 통합

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,18 @@ import { UsersModule } from './users/users.module';
 import { BoardsModule } from './boards/boards.module';
 import { CommentsModule } from './comments/comments.module';
 import { LoggerModule } from './common/logger/loger.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggingInterceptor } from './common/interceptor/logging.interceptor';
 
 @Module({
   imports: [UsersModule, BoardsModule, CommentsModule, LoggerModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor,
+    },
+  ],
 })
 export class AppModule {}
